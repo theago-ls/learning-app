e456b4a3ab987517ea827f0ed2e31919da1e93d6
Thiago Lourenço <thiagosup1@gmail.com>
2022-01-08 00:44:35 -0400

    feat: adds option selection

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, Text, Pressable} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {colors} from '../utils';
 
-const Option = (props: {text: string}) => {
+type OptionsProps = {text: string; isSelected: boolean; onTap: () => void};
+
+const Option = ({text, isSelected, onTap}: OptionsProps) => {
+  const selectedStyles = [
+    {opacity: isSelected ? 0.3 : 1},
+    {opacity: isSelected ? 0 : 1},
+  ];
+
   return (
-    <View style={optionStyles.option}>
-      <Text style={optionStyles.text}>{props.text}</Text>
+    <View style={selectedStyles[0]}>
+      <TouchableOpacity style={optionStyles.option} onPress={onTap}>
+        <Text style={[optionStyles.text, selectedStyles[1]]}>{text}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -24,28 +33,130 @@ const optionStyles = StyleSheet.create({
   },
 });
 
+const options = [
+  {id: 0, word: 'Hause'},
+  {id: 1, word: 'folgen'},
+  {id: 2, word: 'Schaf'},
+  {id: 3, word: 'Bereiden'},
+];
+
+const primaryWords = [
+  {
+    id: 0,
+    word: 'The',
+  },
+  {
+    id: 1,
+    word: 'house',
+  },
+  {
+    id: 2,
+    word: 'is',
+  },
+  {
+    id: 3,
+    word: 'small',
+  },
+];
+
+const learningWords = [
+  {
+    id: 0,
+    word: 'Das',
+  },
+  {
+    id: 1,
+    word: null,
+  },
+  {
+    id: 2,
+    word: 'ist',
+  },
+  {
+    id: 3,
+    word: 'klein',
+  },
+];
+
+const primaryWordId = 1;
+const learningWordId = 1;
+
+const breakPhrase = (
+  words: {id: number; word: string | null}[],
+  pivotId: number,
+) => {
+  const before = words
+    .slice(
+      0,
+      words.findIndex(word => word.id === pivotId),
+    )
+    .map(({word}) => word)
+    .join(' ');
+
+  const after = words
+    .filter(word => word.id > pivotId)
+    .map(word => word.word)
+    .join(' ');
+
+  const pivot = words.find(word => word.id === pivotId)?.word;
+
+  return {before, after, pivot};
+};
+
 export default function Exercise() {
+  const [selected, setSelected] = React.useState<{
+    id: number;
+    word: string;
+  } | null>(null);
+
+  const {
+    before: beforePrimary,
+    after: afterPrimary,
+    pivot: primary,
+  } = breakPhrase(primaryWords, primaryWordId);
+
+  const {before: beforeLearning, after: afterLearning} = breakPhrase(
+    learningWords,
+    learningWordId,
+  );
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.title}>Fill the missing word</Text>
         <Text style={styles.primaryLanguageText}>
-          The{' '}
+          {beforePrimary}{' '}
           <Text style={[styles.primaryLanguageText, styles.primaryWord]}>
-            house
+            {primary}
           </Text>{' '}
-          is small.
+          {afterPrimary}
         </Text>
-        <Text style={styles.learningLanguageText}>Das ________ ist klein.</Text>
-        <View style={styles.options}>
-          <Option text="Hause" />
-          <Option text="folgen" />
-          <Option text="Schaf" />
-          <Option text="Bereiden" />
+        <View style={[styles.learningLanguageContainer]}>
+          <Text style={styles.learningLanguageText}>{beforeLearning}</Text>
+          {selected ? (
+            <TouchableOpacity
+              style={optionStyles.option}
+              onPress={() => setSelected(null)}>
+              <Text style={optionStyles.text}>{selected.word}</Text>
+            </TouchableOpacity>
+          ) : (
+            <Text style={styles.underline}>________</Text>
+          )}
+          <Text style={styles.learningLanguageText}>{afterLearning}</Text>
         </View>
-        <Pressable style={styles.button}>
+        <View style={styles.options}>
+          {options.map(option => (
+            <Option
+              key={option.id}
+              text={option.word}
+              isSelected={selected?.id === option.id}
+              onTap={() => setSelected(option)}
+            />
+          ))}
+        </View>
+        <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Continue</Text>
-        </Pressable>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -82,11 +193,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textDecorationLine: 'underline',
   },
+  learningLanguageContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    height: '15%',
+  },
   learningLanguageText: {
     textAlign: 'center',
     fontSize: 24,
-    textDecorationLine: 'underline',
-    textDecorationStyle: 'dotted',
+    marginHorizontal: 10,
+  },
+  underline: {
+    marginBottom: -12,
   },
   options: {
     display: 'flex',
@@ -96,7 +216,6 @@ const styles = StyleSheet.create({
     width: '55%',
     flexWrap: 'wrap',
     margin: 40,
-    marginBottom: 130,
   },
   button: {
     display: 'flex',
@@ -105,6 +224,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255,255,255,0.4)',
     width: '80%',
     alignSelf: 'center',
+    position: 'absolute',
+    bottom: 80,
   },
   buttonText: {
     textAlign: 'center',
